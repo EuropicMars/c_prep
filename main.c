2f94babd01acb8517100d89e2082009f8685d194
GBednarik <bednarik.gergo@rubin.hu>
2025-06-09 15:24:51 +0200

    second commit

--- main.c
+++ main.c
@@ -5,28 +5,49 @@
 #include <stdbool.h>
 
 
-bool* createLoad(int *size) {
+void shoot(bool *load, int size) 
+{
+    // Simulate shooting at the load
+    for(int i = 0; i < size; i++) 
+    {
+        while (getchar() != '\n');
+
+        if (load[i]) 
+        {
+            printf("Shot at index %d: Live!\n", i);
+        }else 
+        {
+            printf("Shot at index %d: Blank!\n", i);
+        }
+    }
+}
+
+bool* createLoad(int *size) 
+{
     //Generate the load size
     srand(time(0));
     const int min = 4;
     const int max = 10;
     *size = rand() % (max - min + 1) + min;
-    printf("Load size: %d\n", *size);
+
 
     int min_live = 1;
-
     bool *load = (bool*)malloc(*size * sizeof(bool));
-    if (!load) {
+    if (!load) 
+    {
         perror("malloc failed");
         exit(1);
     }
-    for (int i = 0; i < min_live; i++) {
+    for (int i = 0; i < min_live; i++) 
+    {
         load[i] = true;
     }
-    for (int i = min_live; i < *size; i++) {
+    for (int i = min_live; i < *size; i++) 
+    {
         load[i] = rand() % 2;
     }
-    for (int i = *size - 1; i > 0; i--) {
+    for (int i = *size - 1; i > 0; i--) 
+    {
         int j = rand() % (i + 1);
         bool temp = load[i];
         load[i] = load[j];
@@ -37,27 +58,29 @@ bool* createLoad(int *size) {
 }
 
 
-int main() {
-    printf("Setup complete.\n");
+int main() 
+{
+    while (1) 
+    {
+        int size;
+        bool *load = createLoad(&size);
+        printf("Load array: ");
+        for (int i = 0; i < size; i++) {
+            printf("%d ", load[i]);
+        }
+        printf("\n");
 
-    while (1) {
-    int size;
-    bool *load = createLoad(&size);
-    printf("Load array: ");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", load[i]);
-    }
-    printf("\n");
+        int live_count = 0, blank_count = 0;
+        for (int i = 0; i < size; i++) {
+            if (load[i]) live_count++;
+            else blank_count++;
+        }
+        printf("Live count: %d, Blank count: %d\n", live_count, blank_count);
 
-    int live_count = 0, blank_count = 0;
-    for (int i = 0; i < size; i++) {
-        if (load[i]) live_count++;
-        else blank_count++;
-    }
-    printf("Live count: %d, Blank count: %d\n", live_count, blank_count);
+        shoot(load, size);
 
-    free(load);
-    Sleep(3000);
+        free(load);
+        Sleep(3000);
     }
 
     return 0;
